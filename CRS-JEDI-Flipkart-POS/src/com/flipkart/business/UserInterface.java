0913a73592613e5dca1271f64ba5b77798049536
sarthak sadh <33261776+sarthaksadh01@users.noreply.github.com>
2021-08-23 19:23:19 +0530

    Update UserInterface.java

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/business/UserInterface.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/business/UserInterface.java
@@ -1,12 +1,12 @@
 package com.flipkart.business;
 
 public interface UserInterface {
-    public void createUser(String userId,String password,String name);
     public void updatePassword(String userId,String password);
     public void updateName(String userId,String name);
     public void deleteUser(String userId);
     public void login(String userId);
     public void logout();
     public boolean isUserLogin(String userId);
+    public boolean updateGender(String gender);
 
 }

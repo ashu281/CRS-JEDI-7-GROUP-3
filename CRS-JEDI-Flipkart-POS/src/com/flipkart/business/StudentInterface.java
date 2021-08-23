b159c288df26f95fea979d88f05c6db6760f2af5
Pulkit Khurana <khuranapulkit.pk@gmail.com>
2021-08-23 20:03:48 +0530

    Added StudentInterface

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/business/StudentInterface.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/business/StudentInterface.java
@@ -1,4 +1,8 @@
 package com.flipkart.business;
 
 public interface StudentInterface {
+
+    void register(int studentID, String branch, int semester);
+    String viewGradeCard(int studentID, int semester);
+
 }

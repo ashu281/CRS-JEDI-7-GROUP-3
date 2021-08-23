69f019c95db19513b3cbd3fe6fc5fa508b89bcf0
ashu281 <54212175+ashu281@users.noreply.github.com>
2021-08-23 20:25:09 +0530

    Added ProfessorInterface

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/business/ProfessorInterface.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/business/ProfessorInterface.java
@@ -1,4 +1,11 @@
 package com.flipkart.business;
 
+import com.flipkart.bean.Course;
+import com.flipkart.bean.Grade;
+import com.flipkart.bean.Student;
+
 public interface ProfessorInterface {
+    public void addGrade(String studentId, String courseCode, String grade);
+    public Student[] viewEnrolledStudents(int profId,String courseCode);
+    public Course[] getCourses(int profId);
 }

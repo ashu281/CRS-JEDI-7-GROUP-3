1cd75843c48697393dc7651dcedf975719764b25
ashu281 <54212175+ashu281@users.noreply.github.com>
2021-08-23 20:16:04 +0530

    Added Professor Bean Class

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Professor.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Professor.java
@@ -1,4 +1,30 @@
 package com.flipkart.bean;
 
-public class Professor {
+public class Professor extends User{
+
+    private String designation;
+    private String department;
+
+    public Professor(String userId, String name, String gender, String password,String branch, int studentId, int semester) {
+        super(userId, name, gender, password);
+        this.designation = designation;
+        this.department = department;
+    }
+    public String getDesignation() {
+        return designation;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+
 }

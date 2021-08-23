437e4fad273702aaab1c2f66d19e793bcabfd871
Nimisha Gupta <nimishagupta.ig@gmail.com>
2021-08-23 20:17:41 +0530

    Added Admin Interface

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/business/AdminInterface.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/business/AdminInterface.java
@@ -1,4 +1,12 @@
 package com.flipkart.business;
 
+import com.flipkart.bean.Student;
+import com.flipkart.bean.Professor;
+
 public interface AdminInterface {
+	public void addProfessor();
+	public void approveStudents();
+	public void modifyCourses();
+	public void generateGradeCard();
+	
 }

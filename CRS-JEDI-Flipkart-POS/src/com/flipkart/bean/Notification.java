ba38b5c907f5093a458673b754deb6571d6c3e40
setump <75327726+setump@users.noreply.github.com>
2021-08-23 20:37:51 +0530

    Update Notification.java

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Notification.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Notification.java
@@ -1,4 +1,41 @@
 package com.flipkart.bean;
 
 public class Notification {
+
+    float refId;
+    int paymentId;
+    int studentId;
+    String notificationMessage;
+
+    public float getRefId() {
+        return refId;
+    }
+
+    public void setRefId(float refId) {
+        this.refId = refId;
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getNotificationMessage() {
+        return notificationMessage;
+    }
+
+    public void setNotificationMessage(String notificationMessage) {
+        this.notificationMessage = notificationMessage;
+    }
 }

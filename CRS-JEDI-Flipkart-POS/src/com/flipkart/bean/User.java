37aba400f38411b6ec7b32690e1f69da33019a85
sarthak sadh <33261776+sarthaksadh01@users.noreply.github.com>
2021-08-23 16:56:27 +0530

    Update User.java

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/User.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/User.java
@@ -1,4 +1,31 @@
 package com.flipkart.bean;
 
 public class User {
+    private String id;
+    private String password;
+    private String name;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

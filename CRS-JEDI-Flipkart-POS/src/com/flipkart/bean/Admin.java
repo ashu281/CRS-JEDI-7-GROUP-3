b01a68585ee93345e7a319cad3230b88c541b07c
Nimisha Gupta <nimishagupta.ig@gmail.com>
2021-08-24 12:20:01 +0530

    Updated Admin files

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Admin.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Admin.java
@@ -2,12 +2,29 @@ package com.flipkart.bean;
 
 import java.util.Date;
 
+/**
+ *
+ * @author JEDI-07
+ * Admin Class
+ *
+ */
+
 public class Admin extends User{
 	private String adminID;
+
+	/**
+	 * Method to get Admin ID
+	 * @return Admin ID
+	 */
 
 	public String getAdminID() {
 		return adminID;
 	}
+
+	/**
+	 * Method to set Admin ID
+	 * @param Admin ID
+	 */
 
 	public void setAdminID(String adminID) {
 		this.adminID = adminID;

7b4ba0398e3d643ab3c0b8ad1a3f867f928116c8
setump <75327726+setump@users.noreply.github.com>
2021-08-24 21:23:32 +0530

    comments added

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Grade.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Grade.java
@@ -2,52 +2,107 @@ package com.flipkart.bean;
 
 import java.util.HashMap;
 
-import com.flipkart.bean.Course;
-
 public class Grade {
-    String name;
-    int studentId;
+    String courseName;
+    String studentID;
     int semester;
     float CGPA;
     HashMap<String, Integer> grades;
     /*courseCode to gradePoint*/
 
-
-    public String getName() {
-        return name;
+    /**
+     * Method to get Courses name
+     *
+     * @return Course Name
+     */
+    public String getCourseName() {
+        return courseName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * Method to set Courses name
+     *
+     * @param courseName
+     */
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
     }
 
-    public int getStudentId() {
-        return studentId;
+    /**
+     * Method to get studentId
+     *
+     * @return student Id
+     */
+
+    public String getStudentID() {
+        return studentID;
     }
 
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
+    /**
+     * Method to set Courses name
+     *
+     * @param studentID
+     */
+
+    public void setStudentId(String studentID) {
+        this.studentID = studentID;
     }
+
+    /**
+     * Method to get semester number
+     *
+     * @return semester number
+     */
 
     public int getSemester() {
         return semester;
     }
 
+    /**
+     * Method to set semester number
+     *
+     * @param semester : semester number
+     */
+
     public void setSemester(int semester) {
         this.semester = semester;
     }
+
+    /**
+     * Method to get CGPA
+     *
+     * @return CGPA
+     */
 
     public float getCGPA() {
         return CGPA;
     }
 
+    /**
+     * Method to set CGPA
+     *
+     * @param CGPA
+     */
     public void setCGPA(float CGPA) {
         this.CGPA = CGPA;
     }
 
+    /**
+     * Method to get Grades
+     *
+     * @return Hashmap with CourseID as key and Grade point as value
+     */
+
     public HashMap<String, Integer> getGrades() {
         return grades;
     }
+
+    /**
+     * Method to set Grades
+     *
+     * @param grades : Hashmap with CourseID as key and Grade point as value
+     */
 
     public void setGrades(HashMap<String, Integer> grades) {
         this.grades = grades;

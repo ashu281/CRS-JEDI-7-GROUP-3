0ad296e594b9bfceda154b554ece9fe690797636
unknown <ashutoshrathi281@gmail.com>
2021-08-24 13:31:43 +0530

    Added Professor bean comments

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Professor.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Professor.java
@@ -1,5 +1,13 @@
 package com.flipkart.bean;
 
+
+
+/**
+ *
+ * @author JEDI-07
+ * Professor Class
+ *
+ */
 public class Professor extends User{
 
     private String designation;
@@ -12,18 +20,39 @@ public class Professor extends User{
         this.department = department;
         this.profId = profId;
     }
+
+    /**
+     * Method to get Professor Designation
+     * @return designation of professor
+     */
     public String getDesignation() {
         return designation;
     }
 
+
+    /**
+     * Method to get Professor Designation
+     * @return department of professor
+     */
     public String getDepartment() {
         return department;
     }
 
+
+    /**
+     * Method to set Professor Designation
+     * @param designation of professor
+     */
     public void setDesignation(String designation) {
         this.designation = designation;
     }
 
+
+
+    /**
+     * Method to set Professor Department
+     * @param department of professor
+     */
     public void setDepartment(String department) {
         this.department = department;
     }
--- CRS-JEDI-Flipkart-POS/src/com/flipkart/business/ProfessorInterface.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/business/ProfessorInterface.java
@@ -4,6 +4,12 @@ import com.flipkart.bean.Course;
 import com.flipkart.bean.Grade;
 import com.flipkart.bean.Student;
 
+/**
+ *
+ * @author JEDI-07
+ * Interface for Professor Operations
+ *
+ */
 public interface ProfessorInterface {
     public void addGrade(String studentId, String courseCode, String grade);
     public Student[] viewEnrolledStudents(int profId,String courseCode);

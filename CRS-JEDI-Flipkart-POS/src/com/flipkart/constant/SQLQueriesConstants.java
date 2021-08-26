a0e4bea14cfb76a8743b0b78f4c82f03f96dfd8e
abhinavsaxena99 <abhinav.saxena10@gmail.com>
2021-08-26 17:36:21 +0530

    updated "student_reg_courses" table name to "registration"

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/constant/SQLQueriesConstants.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/constant/SQLQueriesConstants.java
@@ -18,23 +18,23 @@ public class SQLQueriesConstants {
     public static final String APPROVE_STUDENT = "update student set isApproved = 1 where studentId = ?";
     public static final String ADD_USER_QUERY = "insert into user(password,role,name,address,gender) values (?, ?, ?, ?,?)";
     public static final String ADD_PROFESSOR = "insert into professor(userId,department, designation) values (?, ?, ?)";
-    public static final String APPROVE_COURSE = "update student_reg_course set isApproved = 1 where studentId = ? and courseId = ?";
+    public static final String APPROVE_COURSE = "update registration set isApproved = 1 where studentId = ? and courseId = ?";
     public static final String ADD_STUDENT="insert into student (branch,semester,userId) values (?,?,?)";
 
     // Student Queries
-    public static final String VIEW_REGISTERED_COURSES=" select * from student_reg_course  inner join course on course.courseId = student_reg_course.courseId where student_reg_course.studentId = ? AND student_reg_course.isApproved = 1";
+    public static final String VIEW_REGISTERED_COURSES=" select * from registration  inner join course on course.courseId = registration.courseId where registration.studentId = ? AND registration.isApproved = 1";
     public static final String VIEW_AVAILABLE_COURSES=" select * from course where semester = ?";
     public static final String GET_STUDID="select studentId from student where userId = ?";
     public static final String IS_APPROVED="select isApproved from student where studentId = ? ";
-    public static final String VIEW_SELECTED_COURSES="select * from student_reg_course  inner join course on course.courseId = student_reg_course.courseId where student_reg_course.studentId = ?";
-    public static final String STUDENT_ADD_COURSE="insert into student_reg_course (studentId,courseId) values ( ? , ?)";
-    public static final String STUDENT_DROP_COURSE = "delete from student_reg_course where courseId = ? AND studentId = ?;";
-    public static final String VIEW_GRADE = "select * from student_reg_course inner join course on course.courseId = student_reg_course.courseId where studentId = ? AND isApproved = 1 AND semester = ?;";
+    public static final String VIEW_SELECTED_COURSES="select * from registration  inner join course on course.courseId = registration.courseId where registration.studentId = ?";
+    public static final String STUDENT_ADD_COURSE="insert into registration (studentId,courseId) values ( ? , ?)";
+    public static final String STUDENT_DROP_COURSE = "delete from registration where courseId = ? AND studentId = ?;";
+    public static final String VIEW_GRADE = "select * from registration inner join course on course.courseId = registration.courseId where studentId = ? AND isApproved = 1 AND semester = ?;";
 
       // Professor Queries
-    public static final String ADD_GRADE="update student_reg_course set grade=? where courseId=? and studentId=?";
+    public static final String ADD_GRADE="update registration set grade=? where courseId=? and studentId=?";
     public static final String GET_COURSES="select * from course where profId=?";
     public static final String GET_PROFID="select profId from professor where userId = ?";
-    public  static  final  String GET_ENROLLED_STUDENTS = "select student_reg_course.studentId,student.userId,user.name from student_reg_course join student on student.studentId = student_reg_course.studentId join user on user.userId = student.userId where courseId = ?";
+    public  static  final  String GET_ENROLLED_STUDENTS = "select registration.studentId,student.userId,user.name from registration join student on student.studentId = registration.studentId join user on user.userId = student.userId where courseId = ?";
 
 }

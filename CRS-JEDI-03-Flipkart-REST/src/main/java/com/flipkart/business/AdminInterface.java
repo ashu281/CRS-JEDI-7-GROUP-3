f8c86f595fa381d1a92044da01137dc67203a653
abhinavsaxena99 <abhinav.saxena10@gmail.com>
2021-08-27 15:10:46 +0530

    added business and dao layers

---
+++ CRS-JEDI-03-Flipkart-REST/src/main/java/com/flipkart/business/AdminInterface.java
@@ -0,0 +1,49 @@
+package com.flipkart.business;
+
+import com.flipkart.exception.CourseNotFoundException;
+import com.flipkart.exception.StudentNotFoundForApprovalException;
+
+/**
+ *
+ * @author JEDI-07
+ * Interface for Admin Operations
+ *
+ */
+
+public interface AdminInterface {
+
+	/**
+	 * Method to add professor
+	 * @param name
+	 * @param gender
+	 * @param password
+	 * @param address
+	 * @param designation
+	 * @param department
+	 */
+	public void addProfessor(String name, String gender, String password, String address, String designation, String department);
+
+	/**
+	 * Method to approve a student
+	 * @param studentId
+	 */
+	public void approveStudent(int studentId);
+
+	/**
+	 * Method to add courses in the catalog
+	 */
+	public void addCourse(String courseName, int instructorID, Integer semester);
+
+	/**
+	 * Method to delete courses in the catalog
+	 * @param courseID
+	 */
+	public void deleteCourse(int courseID);
+
+	/**
+	 * Method to approve student's selected course
+	 * @param studentId
+	 * @param courseId
+	 */
+    void approveCourse(int studentId, int courseId);
+}
---
+++ CRS-JEDI-03-Flipkart-REST/src/main/java/com/flipkart/dao/AdminDaoInterface.java
@@ -0,0 +1,35 @@
+package com.flipkart.dao;
+
+import com.flipkart.exception.CourseNotFoundException;
+import com.flipkart.exception.StudentNotFoundForApprovalException;
+
+public interface AdminDaoInterface {
+
+    /**
+     * Method to add a new professor
+     *
+     */
+    public void addProfessor(String name, String gender, String password, String address, String designation, String department);
+
+    /**
+     * Method to approve a student
+     */
+    public void approveStudent(int studentId) throws StudentNotFoundForApprovalException;
+
+    /**
+     * Method to add courses in the catalog
+     */
+    public void addCourse(String courseName, int instructorID, int semester);
+
+    /**
+     * Method to delete courses in the catalog
+     */
+    public void deleteCourse(int courseID) throws CourseNotFoundException;
+
+    /**
+     * Method to approve student's course
+     * @param studentId
+     * @param courseId
+     */
+    void approveCourse(int studentId, int courseId);
+}

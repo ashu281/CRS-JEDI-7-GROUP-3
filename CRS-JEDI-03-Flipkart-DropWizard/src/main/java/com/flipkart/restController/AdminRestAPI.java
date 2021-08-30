6a44fd00fdd076a1e467de16dae637278e35c68e
setump <75327726+setump@users.noreply.github.com>
2021-08-30 20:29:03 +0530

    added logger statements

--- CRS-JEDI-03-Flipkart-DropWizard/src/main/java/com/flipkart/restController/AdminRestAPI.java
+++ CRS-JEDI-03-Flipkart-DropWizard/src/main/java/com/flipkart/restController/AdminRestAPI.java
@@ -9,6 +9,8 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 
 /**
  * @author Sarthak
@@ -17,8 +19,10 @@ import java.util.Map;
 @Path("/admin")
 public class AdminRestAPI {
 
+    private static Logger logger = Logger.getLogger(AdminRestAPI.class);
 
     AdminInterface adminOperation = new AdminOperation();
+
     /**
      * Method to approve student's course
      */
@@ -27,11 +31,18 @@ public class AdminRestAPI {
     @Path("/approveCourse")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String approveCourse(Map<String,String> params, @HeaderParam("authKey") String authKey) {
-        if(UserAuth.isAdminLogin(authKey) == null){
+    public String approveCourse(Map<String, String> params, @HeaderParam("authKey") String authKey) {
+        logger.debug("Inside approveCourse");
+        logger.debug("authKey is " + authKey);
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            logger.info(entry.getKey() + " " + entry.getValue());
+        }
+        if (UserAuth.isAdminLogin(authKey) == null) {
+            logger.info("Invalid Auth Key");
             return "Access Denied";
         }
-        adminOperation.approveCourse(Integer.parseInt(params.get("studentId")),Integer.parseInt(params.get("courseId")));
+        adminOperation.approveCourse(Integer.parseInt(params.get("studentId")), Integer.parseInt(params.get("courseId")));
+        logger.info("Course Approved");
         return "Course Approved Successfully!";
     }
 
@@ -43,14 +54,21 @@ public class AdminRestAPI {
     @Path("/addProfessor")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String addProfessor(Map<String,String> params, @HeaderParam("authKey") String authKey) {
-        if(UserAuth.isAdminLogin(authKey) == null){
+    public String addProfessor(Map<String, String> params, @HeaderParam("authKey") String authKey) {
+        logger.debug("Inside addProfessor");
+        logger.debug("authKey is " + authKey);
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            logger.info(entry.getKey() + " " + entry.getValue());
+        }
+        if (UserAuth.isAdminLogin(authKey) == null) {
+            logger.info("Invalid Auth Key");
             return "Access Denied";
         }
-        Pair<Integer,Integer> p = adminOperation.addProfessor(params.get("name"),params.get("gender"),params.get("password"),
-                                    params.get("address"),params.get("designation"),params.get("department"));
+        Pair<Integer, Integer> p = adminOperation.addProfessor(params.get("name"), params.get("gender"), params.get("password"),
+                params.get("address"), params.get("designation"), params.get("department"));
 
-        return "Professor "+params.get("name")+" Added! UserID: "+p.getValue()+" ProfId: "+p.getKey();
+        logger.info("Successfully added the new professor");
+        return "Professor " + params.get("name") + " Added! UserID: " + p.getValue() + " ProfId: " + p.getKey();
 
     }
 
@@ -61,14 +79,20 @@ public class AdminRestAPI {
     @Path("/approveStudent")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String approveStudent(Map<String,String> params, @HeaderParam("authKey") String authKey) {
+    public String approveStudent(Map<String, String> params, @HeaderParam("authKey") String authKey) {
 
-        if(UserAuth.isAdminLogin(authKey) == null){
+        logger.debug("Inside approveStudent");
+        logger.debug("authKey is " + authKey);
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            logger.info(entry.getKey() + " " + entry.getValue());
+        }
+        if (UserAuth.isAdminLogin(authKey) == null) {
+            logger.info("Invalid Auth Key");
             return "Access Denied";
         }
-
+        logger.info("Successfully approved the new student");
         adminOperation.approveStudent(Integer.parseInt(params.get("studentId")));
-        return "Student "+params.get("studentId")+" Approved!";
+        return "Student " + params.get("studentId") + " Approved!";
 
 
     }
@@ -81,12 +105,12 @@ public class AdminRestAPI {
     @Path("/deleteCourse")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String deleteCourse(Map<String,String> params, @HeaderParam("authKey") String authKey) {
-        if(UserAuth.isAdminLogin(authKey) == null){
+    public String deleteCourse(Map<String, String> params, @HeaderParam("authKey") String authKey) {
+        if (UserAuth.isAdminLogin(authKey) == null) {
             return "Access Denied";
         }
         adminOperation.deleteCourse(Integer.parseInt(params.get("courseId")));
-        return "Course "+params.get("courseId")+" deleted!";
+        return "Course " + params.get("courseId") + " deleted!";
 
     }
 
@@ -97,12 +121,12 @@ public class AdminRestAPI {
     @Path("/addCourse")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String addCourse(Map<String,String> params, @HeaderParam("authKey") String authKey) {
-        if(UserAuth.isAdminLogin(authKey) == null){
+    public String addCourse(Map<String, String> params, @HeaderParam("authKey") String authKey) {
+        if (UserAuth.isAdminLogin(authKey) == null) {
             return "Access Denied";
         }
         int cid = adminOperation.addCourse(params.get("courseName"), Integer.parseInt(params.get("instructorId")), Integer.parseInt(params.get("semester")));
-        return "Course "+params.get("courseName")+" Added! CourseId: "+cid;
+        return "Course " + params.get("courseName") + " Added! CourseId: " + cid;
 
     }
 }
